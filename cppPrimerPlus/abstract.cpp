3dc00688c55c17a6f07ab4bf4c17fd576f22ca57
moon <271586852@qq.com>
2024-01-21 18:11:15 +0800

    删除不必要文件

---
+++ cppPrimerPlus/abstract.cpp
@@ -0,0 +1,49 @@
+//抽象类的定义和使用示例
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual double getArea() const = 0;
+    virtual double getPerimeter() const = 0;
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle(double width, double height) : m_width(width), m_height(height) {}
+    double getArea() const override {
+        return m_width * m_height;
+    }
+    double getPerimeter() const override {
+        return 2 * (m_width + m_height);
+    }
+private:
+    double m_width;
+    double m_height;
+};
+
+class Circle : public Shape {
+public:
+    Circle(double radius) : m_radius(radius) {}
+    double getArea() const override {
+        return 3.14 * m_radius * m_radius;
+    }
+    double getPerimeter() const override {
+        return 2 * 3.14 * m_radius;
+    }
+private:
+    double m_radius;
+};
+
+int main() {
+    Rectangle rect(3, 4);
+    Circle circle(5);
+    cout << "Rectangle area: " << rect.getArea() << endl;
+    cout << "Rectangle perimeter: " << rect.getPerimeter() << endl;
+    cout << "Circle area: " << circle.getArea() << endl;
+    cout << "Circle perimeter: " << circle.getPerimeter() << endl;
+    return 0;
+}
+
+
+
---
+++ cutPoint.cpp
@@ -0,0 +1,24 @@
+// 写一个cpp代码，代码里提供关于断点的测试教学，教学要全面
+
+#include <iostream>
+
+
+int main() {
+    // 在标准输出中打印 "Hello, world!"
+    std::cout << "Hello, world!" << std::endl;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int e = 5;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "c = " << c << std::endl;
+    std::cout << "d = " << d << std::endl;
+    std::cout << "e = " << e << std::endl;
+    return 0;
+}
+
+// 此文件编译运行将输出 "Hello, world!"。
+
+
